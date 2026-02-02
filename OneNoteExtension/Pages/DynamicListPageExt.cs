7eb659bd1a8b9f2735c6fd92d953d584b31b4b66
Odotocodot <48138990+Odotocodot@users.noreply.github.com>
2026-02-02 17:13:50 +0000

    create `DynamicListPageExt`

---
+++ OneNoteExtension/Pages/DynamicListPageExt.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CommandPalette.Extensions;
+
+namespace OneNoteExtension.Pages
+{
+    internal abstract partial class DynamicListPageExt : ListPageExt, IDynamicListPage
+    {
+        public override string SearchText
+        {
+            get => base.SearchText;
+            set
+            {
+                SetSearchNoUpdate(value);
+                UpdateSearchText(base.SearchText, value);
+            }
+        }
+
+        public abstract void UpdateSearchText(string oldSearch, string newSearch);
+    }
+}
