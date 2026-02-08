19ca459dd0bc4c330d4cd7e71da85d5c937f73b9
Odotocodot <48138990+Odotocodot@users.noreply.github.com>
2026-02-08 20:06:13 +0000

    move icons into new folder

--- OneNoteExtension/Helpers/Icons.cs
+++ OneNoteExtension/Helpers/Icons.cs
@@ -9,10 +9,11 @@ namespace OneNoteExtension.Helpers;
 
 internal static class Icons
 {
-    public static IconInfo Logo { get; } = IconHelpers.FromRelativePath($"Assets\\logo.svg");
-    public static IconInfo OneNote => IconHelpers.FromRelativePath("Assets\\onenote.svg");
-    public static IconInfo Invalid => IconHelpers.FromRelativePath("Assets\\warning.light.svg");
-    public static IconInfo Open { get; } = new IconInfo("\ue8a7"); //OpenInNewWindow
+    private const string _path = "Assets\\Icons";
+    public static IconInfo Logo { get; } = IconHelpers.FromRelativePath($"{_path}\\logo.svg");
+    public static IconInfo OneNote => IconHelpers.FromRelativePath($"{_path}\\onenote.svg");
+    public static IconInfo Invalid => IconHelpers.FromRelativePath($"{_path}\\warning.light.svg");
+    public static IconInfo Open { get; } = new ("\ue8a7"); //OpenInNewWindow
     public static IconInfo RecentPage { get; } = FromAssetName("page_recent");
     public static IconInfo OneNoteExplorer { get; } = FromAssetName("notebook_explorer");
     public static IconInfo Page { get; } = FromAssetName("page");
@@ -27,7 +28,7 @@ internal static class Icons
     public static IconInfo Title { get; } = new("\uf714");
     public static IconInfo SearchPages { get; } = FromAssetName("page_search");
 
-    private static IconInfo FromAssetName(string assetName) => IconHelpers.FromRelativePath($"Assets\\{assetName}.color.svg");
+    private static IconInfo FromAssetName(string assetName) => IconHelpers.FromRelativePath($"{_path}\\{assetName}.color.svg");
 
     public static IconInfo GetIcon(IOneNoteItem item) => item switch
     {
@@ -42,19 +43,19 @@ internal static class Icons
     {
         if (!color.HasValue)
         {
-            return IconHelpers.FromRelativePath($"Assets\\{assetName}.color.svg");
+            return IconHelpers.FromRelativePath($"{_path}\\{assetName}.color.svg");
         }
 
         var baseDir = AppDomain.CurrentDomain.BaseDirectory;
         var colorString = ColorTranslator.ToHtml(color.Value);
 
-        var file = Path.Combine(baseDir, "Assets", "Generated", $"{assetName}.{colorString[1..]}.svg");
+        var file = Path.Combine(baseDir, _path, "Generated", $"{assetName}.{colorString[1..]}.svg");
         if (File.Exists(file))
         {
             return new IconInfo(file);
         }
 
-        var baseImage = Path.Combine(baseDir, "Assets", $"{assetName}.dark.svg");
+        var baseImage = Path.Combine(baseDir, _path, $"{assetName}.dark.svg");
         var root = XElement.Load(baseImage);
 
         const string SvgNamespace = "http://www.w3.org/2000/svg";
